bc36b01dc25a0af3adf917f45d5c314037e21d0b
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-10-30 19:47:11 +0100

    Update script-2.sh

--- script-2.sh
+++ script-2.sh
@@ -4,4 +4,29 @@ rpm-ostree upgrade
 
 rpm-ostree install g++ avahi-compat-libdns_sd-devel cmake discord gnome-tweaks gstreamer1-devel gstreamer1-libav gstreamer1-plugins-base-devel htop libplist-devel make neofetch openssl-devel steam wine
 
-systemctl reboot
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.usebottles.bottles
+
+wget https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer -P  ~/Downloads/
+chmod +x ~/Downloads/gnome-shell-extension-installer
+~/Downloads/gnome-shell-extension-installer --yes --restart-shell 19 517 4099 1319 2890 
+
+mkdir ~/.themes
+mkdir ~/.icons
+
+wget https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic-darker-v40.tar.xz -P ~/Downloads/
+wget https://github.com/zayronxio/Zafiro-icons/releases/download/1.3/Zafiro-Icons-Dark.tar.xz -P ~/Downloads/
+
+tar -xf ~/Downloads/Nordic-darker-v40.tar.xz -C ~/.themes
+tar -xf ~/Downloads/Zafiro-Icons-Dark.tar.xz -C ~/.icons
+
+git clone https://github.com/EliverLara/Nordic ~/.icons/Nordic
+mv ~/.icons/Nordic/kde/cursors/Nordic-cursors ~/.icons/
+
+gsettings set org.gnome.desktop.interface gtk-theme "Nordic-darker-v40"
+gsettings set org.gnome.desktop.wm.preferences theme "Nordic-darker-v40"
+gsettings set org.gnome.desktop.interface icon-theme "Zafiro-Icons-Dark"
+gsettings set org.gnome.desktop.interface cursor-theme "Nordic-cursors"
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:appmenu'
+
+echo"Open the Extensions application and enable all extensions, then go to settings of User Themes and pick Nordic-darker-v40. After that go into the Settings application and pick Dark in Appearance -> Styles, then reboot"
