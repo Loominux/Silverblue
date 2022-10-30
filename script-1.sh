a5a25f4c484f326eb3d2b960a776e549b1ab10c9
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-10-30 19:44:22 +0100

    Update script-1.sh

--- script-1.sh
+++ script-1.sh
@@ -1,3 +1,7 @@
+rpm-ostree cancel
+
+rpm-ostree upgrade
+
 sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 systemctl reboot
