92d6ef6a13d740e94fb2fb958d607e6a4578fa50
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-10-30 19:44:40 +0100

    Update script-2.sh

--- script-2.sh
+++ script-2.sh
@@ -1,3 +1,7 @@
+rpm-ostree cancel
+
+rpm-ostree upgrade
+
 rpm-ostree install g++ avahi-compat-libdns_sd-devel cmake discord gnome-tweaks gstreamer1-devel gstreamer1-libav gstreamer1-plugins-base-devel htop libplist-devel make neofetch openssl-devel steam wine
 
 systemctl reboot
