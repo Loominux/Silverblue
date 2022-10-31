e69e540ca747c7c1e27540d8f4e38fb7b46012bd
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-10-31 22:48:50 +0100

    Update script-1.sh

--- script-1.sh
+++ script-1.sh
@@ -2,6 +2,10 @@ rpm-ostree cancel
 
 rpm-ostree upgrade
 
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+rpm-ostree cancel
 sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 systemctl reboot
