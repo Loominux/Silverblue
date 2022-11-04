97dd4ee4c52795f42f0abc28470a96a2efae8825
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-11-04 01:39:06 +0100

    Update script-2.sh

--- script-2.sh
+++ script-2.sh
@@ -2,7 +2,7 @@ rpm-ostree cancel
 
 rpm-ostree upgrade
 
-rpm-ostree install g++ avahi-compat-libdns_sd-devel cmake discord gnome-tweaks gstreamer1-devel gstreamer1-libav gstreamer1-plugins-base-devel htop libplist-devel make neofetch openssl-devel steam wine
+rpm-ostree install g++ avahi-compat-libdns_sd-devel cmake discord gnome-tweaks gstreamer1-devel gstreamer1-libav gstreamer1-plugins-base-devel htop libplist-devel make neofetch openssl-devel steam wine openssl
 
 flatpak install flathub com.usebottles.bottles
 
