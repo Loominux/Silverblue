e52c0d6bf27f6ea2775e2d53ba8eb93a9a21514e
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-11-16 21:31:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@ rpm-ostree cancel
 
 rpm-ostree upgrade
 
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 rpm-ostree install gnome-tweaks htop neofetch wine openssl
 
 flatpak install flathub com.valvesoftware.Steam com.discordapp.Discord
