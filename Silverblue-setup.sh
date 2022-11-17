0b7693304ad521305f6fe565f3e5058f52c379ea
Paul Weber <115210873+Loominux@users.noreply.github.com>
2022-11-17 01:03:57 +0100

    Update Silverblue-setup.sh

--- Silverblue-setup.sh
+++ Silverblue-setup.sh
@@ -1,6 +1,6 @@
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo
 sudo flatpak remote-modify flathub --enable
-flatpak install flathub com.valvesoftware.Steam com.discordapp.Discord com.valvesoftware.Steam.CompatibilityTool.Proton-GE --assumeyes
+flatpak install flathub com.valvesoftware.Steam com.discordapp.Discord com.valvesoftware.Steam.CompatibilityTool.Proton-GE --verbose --assumeyes
 
 
 rpm-ostree cancel
